cd05868be9aa7fa45dafd4cc17e5e6ddc9a33c88
kamhar ngado <52423991+kamhar-ngado@users.noreply.github.com>
2019-11-04 01:51:49 +0700

    nothing happen just fix my typo

--- linedetection.py
+++ linedetection.py
@@ -10,6 +10,5 @@ gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
 #show the image as grayscale
 cv2.imshow('result', gray)
 #the image in form gray scale will appear "..." in mili second "0" is unlimited
-cv2.waitKey(5000)
+cv2.waitKey(0)
 #show the image in matrix
-print(image)
