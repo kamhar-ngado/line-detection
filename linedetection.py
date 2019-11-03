1afeaf56a9b3179ba5fa1636a5e749f4671dea0a
kamhar ngado <52423991+kamhar-ngado@users.noreply.github.com>
2019-11-04 02:54:51 +0700

    Make tidy our image using canny detection
    Nothing happens so keep going I am on my way to find the lane detection

--- linedetection.py
+++ linedetection.py
@@ -1,16 +1,24 @@
 import cv2
 import numpy as np
 
+#declare canny as image
+def canny(image):
+    #run the image to grayscale mode
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    #add the gausian blur to smoth the image and reduce the noisy using 5x5 kernel is good one
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+    #apply canny canny detection on my smooth image to reduce the noise using treshold
+    canny = cv2.Canny(blur, 50, 150)
+    #we back to canny method again
+    return canny
+
 #read the image from left window
 image = cv2.imread('test_image.jpg')
 #analize the image using numpy
 lane_image = np.copy(image)
-#run the image to grayscale mode
-gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
-#add the gausian blur to smoth the image and reduce the noisy using 5x5 kernel is good one
-blur = cv2.GaussianBlur(gray, (5,5), 0)
-#apply canny canny detection on my smooth image to reduce the noise using treshold
-canny = cv2.Canny(blur, 50, 150)
+#apply canny detect on 'test image.jpg'
+canny = canny(lane_image)
+
 #show the image as grayscale
 cv2.imshow('result', canny)
 #the image in form gray scale will appear "..." in mili second "0" is unlimited
