9597d87fd46696350f44249ca6f786dfbf3de323
kamhar ngado <52423991+kamhar-ngado@users.noreply.github.com>
2019-11-04 00:06:19 +0700

    change the img to grayscale
    this is the first step. I  change the lane image to grayscale to analyze furthe

--- linedetection.py
+++ linedetection.py
@@ -1,2 +1,15 @@
-msg = 'hello world'
-print(msg)
+import cv2
+import numpy as np
+
+#read the image from left window
+image = cv2.imread('test_image.jpg')
+#analize the image using numpy
+lane_image = np.copy(image)
+#run the image to grayscale mode
+gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+#show the image as grayscale
+cv2.imshow('result', gray)
+#the image in form gray scale will appear "..." in mili second "0" is unlimited
+cv2.waitKey(5000)
+#show the image in matrix
+print(image)
