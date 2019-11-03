d957f756a9886a84bd228b2ae13f2d7bc069a730
kamhar ngado <52423991+kamhar-ngado@users.noreply.github.com>
2019-11-04 02:34:22 +0700

    Just add Canny detection
    Canny detection using to reduce the noise by using threshold method

--- linedetection.py
+++ linedetection.py
@@ -7,8 +7,12 @@ image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
 #run the image to grayscale mode
 gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+#add the gausian blur to smoth the image and reduce the noisy using 5x5 kernel is good one
+blur = cv2.GaussianBlur(gray, (5,5), 0)
+#apply canny canny detection on my smooth image to reduce the noise using treshold
+canny = cv2.Canny(blur, 50, 150)
 #show the image as grayscale
-cv2.imshow('result', gray)
+cv2.imshow('result', canny)
 #the image in form gray scale will appear "..." in mili second "0" is unlimited
 cv2.waitKey(0)
 #show the image in matrix
