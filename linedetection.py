10572fdea95ea35d6efc7a22e866b9ca27b5b178
kamhar ngado <52423991+kamhar-ngado@users.noreply.github.com>
2019-11-17 01:42:38 +0700

    I fix cannot read the video
    however, I gotta one issue again that is when I try to open the video in real-time until now I don't have the idea. hopefully, in this forum , someone can help me.

--- linedetection.py
+++ linedetection.py
@@ -1,116 +1,93 @@
 import cv2
 import numpy as np
 
-def make_coordinates(image, line_parameters):
-    slope, intercept = line_parameters
-    print(image.shape)
-    y1 = image.shape[0]
-    y2 = int(y1*(3/5))
-    x1 = int((y1-intercept)/slope)
-    x2 = int((y2-intercept)/slope)
-    return np.array([x1, y1, x2, y2])
+
+def make_points(image, line):
+    slope, intercept = line
+    y1 = int(image.shape[0])  # bottom of the image
+    y2 = int(y1*3/5)         # slightly lower than the middle
+    x1 = int((y1 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope)
+    return [[x1, y1, x2, y2]]
 
 
-def avarage_slope_intercept(image, lines):
+def average_slope_intercept(image, lines):
     left_fit = []
     right_fit = []
+    if lines is None:
+        return None
     for line in lines:
-        x1, y1, x2, y2 = line.reshape(4)
-        parameters = np.polyfit((x1, x2), (y1, y2), 1)
-        slope = parameters[0]
-        intercept = parameters[1]
-        if slope < 0:
-            left_fit.append((slope, intercept))
-        else:
-            right_fit.append((slope, intercept))
-    left_fit_avarage = np.average(left_fit, axis=0)
-    right_fit_avarage = np.average(right_fit, axis=0)
-    left_line = make_coordinates(image, left_fit_avarage)
-    right_line = make_coordinates(image, right_fit_avarage)
-    return np.array([left_line, right_line])
+        for x1, y1, x2, y2 in line:
+            fit = np.polyfit((x1, x2), (y1, y2), 1)
+            slope = fit[0]
+            intercept = fit[1]
+            if slope < 0:  # y is reversed in image
+                left_fit.append((slope, intercept))
+            else:
+                right_fit.append((slope, intercept))
+    # add more weight to longer lines
+    left_fit_average = np.average(left_fit, axis=0)
+    right_fit_average = np.average(right_fit, axis=0)
+    left_line = make_points(image, left_fit_average)
+    right_line = make_points(image, right_fit_average)
+    averaged_lines = [left_line, right_line]
+    return averaged_lines
 
 
-# declare canny as image
-def canny(image):
-    # run the image to grayscale mode
-    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    # add the gausian blur to smoth the image and reduce the noisy using 5x5 kernel is good one
-    blur = cv2.GaussianBlur(gray, (5, 5), 0)
-    # apply canny canny detection on my smooth image to reduce the noise using treshold
-    canny = cv2.Canny(blur, 50, 150)
-    # we back to canny method again
+def canny(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    kernel = 5
+    blur = cv2.GaussianBlur(gray, (kernel, kernel), 0)
+    canny = cv2.Canny(gray, 50, 150)
     return canny
 
 
-def display_lines(image, lines):
-    # using zeros_like will  appear the image in zero intensity
-    line_image = np.zeros_like(image)
-    # if it is not empty
+def display_lines(img, lines):
+    line_image = np.zeros_like(img)
     if lines is not None:
-        # using looping for print array x1, x2, x3, x4
         for line in lines:
-            #rebulding our image
-            x1, y1, x2, y2 = line.reshape(4)
-            # change the line from 2D to 1D
-            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+            for x1, y1, x2, y2 in line:
+                cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
     return line_image
 
 
-# define specific area we wnat to calculate in threshol mode
-def region_of_interset(image):
-    # the heightness of our image
-    height = image.shape[0]
-    # this is we get from the violet image we calculate the line in the line road
-    polygons = np.array([
-        [(200, height), (1100, height), (550, 250)]
-    ])
-    # make our image totally black that only have the triangle that following the line road
-    mask = np.zeros_like(image)
-    # our line is white
-    cv2.fillPoly(mask, polygons, 255)
-    # add bitwise operator in our image
-    masked_image = cv2.bitwise_and(image, mask)
-    # back to masked image again
+def region_of_interest(canny):
+    height = canny.shape[0]
+    width = canny.shape[1]
+    mask = np.zeros_like(canny)
+
+    triangle = np.array([[
+        (200, height),
+        (550, 250),
+        (1100, height), ]], np.int32)
+
+    cv2.fillPoly(mask, triangle, 255)
+    masked_image = cv2.bitwise_and(canny, mask)
     return masked_image
 
 
-# ## read the image from left window
 # image = cv2.imread('test_image.jpg')
-# ## analyze the image using numpy
 # lane_image = np.copy(image)
-# ## apply canny detect on 'test image.jpg'
-# canny_image = canny(lane_image)
-# croped_image = region_of_interset(canny_image)
-# ## Apply hough transformation formula
-# lines = cv2.HoughLinesP(croped_image, 2, np.pi / 180, 100, np.array([]), minLineLength=40, maxLineGap=5)
-# ##..
-# avaraged_lines = avarage_slope_intercept(lane_image, lines)
-# ##display the line in the image
-# line_image = display_lines(lane_image, avaraged_lines)
-# ##combina original image with black hough transformation
-# combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
-# ##display the combo image
-# cv2.imshow("result", combo_image)
-# ## the image in will display on the screen
-# cv2.waitKey(0)
+# lane_canny = canny(lane_image)
+# cropped_canny = region_of_interest(lane_canny)
+# lines = cv2.HoughLinesP(cropped_canny, 2, np.pi/180, 100, np.array([]), minLineLength=40,maxLineGap=5)
+# averaged_lines = average_slope_intercept(image, lines)
+# line_image = display_lines(lane_image, averaged_lines)
+# combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 0)
 
+#
 cap = cv2.VideoCapture("test2.mp4")
 while(cap.isOpened()):
-    -, frame = cap.read()
+    _, frame = cap.read()
     canny_image = canny(frame)
-    croped_image = region_of_interset(canny_image)
-    # ## Apply hough transformation formula
-    lines = cv2.HoughLinesP(croped_image, 2, np.pi / 180, 100, np.array([]), minLineLength=40, maxLineGap=5)
-    # ##..
-    avaraged_lines = avarage_slope_intercept(frame, lines)
-    # ##display the line in the image
-    line_image = display_lines(frame, avaraged_lines)
-    # ##combina original image with black hough transformation
+    cropped_canny = region_of_interest(canny_image)
+    lines = cv2.HoughLinesP(cropped_canny, 2, np.pi/180,
+                            100, np.array([]), minLineLength=40, maxLineGap=5)
+    averaged_lines = average_slope_intercept(frame, lines)
+    line_image = display_lines(frame, averaged_lines)
     combo_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
-    # ##display the combo image
     cv2.imshow("result", combo_image)
-    # ## the image in will display on the screen
-    cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
 cv2.destroyAllWindows()
